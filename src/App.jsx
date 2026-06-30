943ff48135c9d565780ff6fb99d45b9164089995
mrvelikiyai <mrvelikiy.ai@gmail.com>
2026-07-01 00:02:30 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
-
+import Mortgage from './components/Mortgage.jsx'
 export default function App() {
   return (
     <div className="min-h-screen bg-slate-50 font-sans antialiased text-slate-900">
